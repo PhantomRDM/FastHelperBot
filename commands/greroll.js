86f32b4815fb4d0df2a72f89ce535c4d384f69f5
PhantomRDM <55496233+PhantomRDM@users.noreply.github.com>
2020-10-20 18:54:26 +0800

    修復控制台異常錯誤

--- commands/greroll.js
+++ commands/greroll.js
@@ -2,32 +2,24 @@ const ms = require('ms');
 
 exports.run = async (client, message, args) => {
 
-    // If the member doesn't have enough permissions
     if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
         return message.channel.send(':x: 你沒有權限可以重新抽出獲獎者。');
     }
 
-    // If no message ID or giveaway name is specified
     if(!args[0]){
         return message.channel.send(':x: 語法錯誤!你必須打出一個有效的訊息ID!');
     }
 
-    // try to found the giveaway with prize then with ID
     let giveaway = 
-    // Search with giveaway prize
     client.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ')) ||
-    // Search with giveaway ID
     client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]);
 
-    // If no giveaway was found
     if(!giveaway){
         return message.channel.send('找不到指定的抽獎活動 `'+ args.join(' ') +'`.');
     }
 
-    // Reroll the giveaway
     client.giveawaysManager.reroll(giveaway.messageID)
     .then(() => {
-        // Success message
         message.channel.send('獎品重新抽出獲獎者!');
     })
     .catch((e) => {
